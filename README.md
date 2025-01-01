2ec294457b3db0aa9c2bff3dcd544f7995b3e08f
David Banks <BanksySan@googlemain.com>
2025-01-01 19:35:48 +0000

    Demo with solid walls, rather than tiled walls created.  Bug still reporducable.

--- README.md
+++ README.md
@@ -16,3 +16,7 @@ When a sprite is rotating, the collision detection goes crazy.
   4. `kCollisionTypeBounce`
 
 Bump the sprite into the walls.  With any collision type other than `kCollisionTypeOverlap` the sprite will perform as expected when rotation is disabled, but will tunnel occasionally tunnel through it when it is set to any of the other types.
+
+* Pressing `Left` or `Right` will swap the walls from tiled, individual sprites to single, long sprites.
+
+*NOTE*  We can reproduce the behaviour in both scenarios.
--- src/main.lua
+++ src/main.lua
@@ -9,15 +9,18 @@ import "Player"
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
+local playerSpeed = 1
+
+local xOffset <const> = 64
+local yOffset <const> = 32
+
+local tileWidth <const> = 16
+local tileHeight <const> = 16
+
+local xCount <const> = 12
+local yCount <const> = 12
+
 function createTiledWall()
-    local tileWidth <const> = 16
-    local tileHeight <const> = 16
-
-    local xCount <const> = 12
-    local yCount <const> = 12
-
-    local xOffset <const> = 64
-    local yOffset <const> = 32
 
     local xStop = xOffset + (tileWidth * (xCount - 1))
     local xStep = tileWidth
@@ -34,11 +37,37 @@ function createTiledWall()
     for j = yOffset + tileHeight, yStop, yStep do
         Wall(xOffset, j, tileWidth, tileHeight)
     end
+
+    Player(124, 124, playerSpeed, false)
 end
 
-createTiledWall()
-Player(124, 124, 1, false)
+function createSolidWall()
+    Wall(xOffset, yOffset, tileWidth * xCount, tileHeight)
+    Wall(xOffset, yOffset + tileHeight, tileWidth, (yCount - 1) * tileHeight)
+    Player(124, 124, playerSpeed, false)
+end
+
+local gameModes = {
+    createTiledWall,
+    createSolidWall
+}
+
+local currentMode = 1
+
+gameModes[currentMode]()
+
 function pd:update()
     gfx.clear()
     gfx.sprite.update()
+
+    if (pd.buttonJustPressed(pd.kButtonLeft) or pd.buttonJustPressed(pd.kButtonRight)) then
+        if (currentMode == 1) then
+            currentMode = 2
+        else
+            currentMode = 1
+        end
+        gfx.sprite.removeAll()
+        gameModes[currentMode]()
+    end
 end
+
